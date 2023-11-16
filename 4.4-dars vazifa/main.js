85de165dbfec6e4995ba47e0e14c167b3d1d2737
Khakimov18 <147815294+Khakimov18@users.noreply.github.com>
2023-11-16 23:23:34 +0500

    Add files via upload

---
+++ 4.4-dars vazifa/main.js
@@ -0,0 +1,103 @@
+// 1-mashq(for): 100 dan 1 gacha bo'lgan sonlarni while lopp bilan chiqar
+// for (let i = 100; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 1-10 gacha juftlarini chiqarish
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 1-10gacha toqlarni chiqarish
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+// 1-10gacha bo'lgan sonlarni  yig'indisini chiqarish
+// let yigindi = 0;
+// for (let i = 1; i <= 10; i++) {
+//   yigindi = yigindi + i;
+// }
+// console.log(yigindi);
+
+// 7 karra jadvalini chiqaring.
+// let karra = "";
+// for (let i = 1; i <= 10; i++) {
+//   karra = karra + i + " x 7 = " + i * 7 + "\n";
+// }
+// console.log(karra);
+// let karra = "";
+// karra = karra + "\n\n";
+// for (let i = 1; i < 10; i++) {
+//   let ichkiKarra = "";
+//   for (let z = 2; z < 6; z++) {
+//     ichkiKarra = ichkiKarra + "\t" + z + " x " + i + " = " + z * i;
+//   }
+//   karra = karra + ichkiKarra + "\n";
+// }
+// karra = karra + "\n\n";
+// for (let i = 1; i < 10; i++) {
+//   let ichkiKarra = "";
+//   for (let z = 6; z < 10; z++) {
+//     ichkiKarra = ichkiKarra + "\t" + z + " x " + i + " = " + z * i;
+//   }
+//   karra = karra + ichkiKarra + "\n";
+// }
+
+// console.log(karra);
+
+// 10 dan katta 30 dan kichik toq sonlar
+// for (let i = 10; i <= 30; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+//for
+// let k = 5;
+// let n = 10;
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+
+//for
+// let a = 10;
+// let b = 20;
+// let jami = 0;
+// for (let i = a; i <= b; i++) {
+//   jami++;
+//   console.log(i);
+// }
+// console.log("jami sonlar " + jami + " ta");
+
+//for
+// let a = 10;
+// let b = 20;
+// let jami = 0;
+// for (let i = b - 1; i > a; i--) {
+//   jami++;
+//   console.log(i);
+// }
+// console.log("jami sonlar " + jami + " ta");
+
+//for
+// let a = 10;
+// let b = 20;
+// let sum = 0;
+// for (let i = a; i <= b; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+//for
+// let a = 10;
+// let b = 20;
+// let sum = 1;
+// for (let i = a; i <= b; i++) {
+//   sum = sum * i;
+// }
+// console.log(sum);
